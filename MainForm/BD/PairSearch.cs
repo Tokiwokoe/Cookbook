f0d71080f1e2f6a8a40a4195a09ee6f83a9cce26
levkindanil <levkindanil@gmail.com>
2022-12-09 17:09:03 +0300

    Разработка интеграции бд (3)

--- MainForm/BD/PairSearch.cs
+++ MainForm/BD/PairSearch.cs
@@ -9,6 +9,13 @@ namespace bd
     // Создана система поиска
     internal class PairSearch
     {
-        
+        public string filter;
+        public string textSearch;
+        public PairSearch(string f, string t)
+        {
+
+            filter = f;
+            textSearch = t;
+        }
     }
 }
