35dd49e96824bc03d7373e36b3bc2af217fbcc52
levkindanil <levkindanil@gmail.com>
2022-12-09 16:54:39 +0300

    Разработка интеграции бд (2)

--- MainForm/BD/Recipe.cs
+++ MainForm/BD/Recipe.cs
@@ -8,6 +8,79 @@ namespace bd
 {
     public class Recipe
     {
+        private int id;
+        private string name;
+        private string category;
+        private string ingredients;
+        private string guide;
+        private string marklike;
+        private string time;
+        private bool star;
+        private string markdif;
+        private byte[] pic;
+
+        public Recipe(int id, string category, string ingredients, string time, string guide, string marklike, string name, string markdif, bool star)
+        {
+            this.id = id;
+            this.name = name;
+            this.category = category;
+            this.ingredients = ingredients;
+            this.guide = guide;
+            this.marklike = marklike;
+            this.markdif = markdif;
+            this.star = star;
+            this.time = time;
+        }
+        public string Time
+        {
+            get { return time; }
+            set { time = value; }
+        }
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Category
+        {
+            get { return category; }
+            set { category = value; }
+        }
+        public string Ingredients
+        {
+            get { return ingredients; }
+            set { ingredients = value; }
+        }
+        public string Guide
+        {
+            get { return guide; }
+            set { guide = value; }
+        }
+        public string Marklike
+        {
+            get { return marklike; }
+            set { marklike = value; }
+        }
+        public string Markdif
+        {
+            get { return markdif; }
+            set { markdif = value; }
+        }
+        public bool Star
+        {
+            get { return star; }
+            set { star = value; }
+        }
+        public byte[] Pic
+        {
+            get { return pic; }
+            set { pic = value; }
+        }
 
     }
 }
