8f9c19a62663ef97bd295005ae4a68ecca350ac5
soFiyaBRS <sofiko110803@gmail.com>
2022-12-10 15:00:22 +0300

    Небольшие изменения

--- MainForm/LanguagesForAddingRecipe.cs
+++ MainForm/LanguagesForAddingRecipe.cs
@@ -14,11 +14,16 @@ namespace MainForm
         //Page "AddRec"
         static public string[] categoriesRu = new string[] { "Другое", "Закуски", "Салаты", "Первые блюда", "Вторые блюда", "Соусы", "Выпечка", "Напитки", "Десерты" };
 
+        static public string[] categoriesEn = new string[] { "Other", "Snacks", "Salads", "First courses", "Main courses", "Sauces", "Pastries", "Drinks", "Desserts" };
 
         static public string RuRu = "Русский";
 
+        static public string EnEn = "English";
+
 
         static public string haveSomeRecRu = "В этом разделе нет рецептов.";
+
+        static public string haveSomeRecEn = "There are no recipes in this section.";
 
     }
 }
