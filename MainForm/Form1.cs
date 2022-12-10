56f9248b78b63d0a07049bd8a48e8cad1107ed2b
Siberanen <siberanen@gmail.com>
2022-12-10 19:11:53 +0300

    Интеграция изображений в программу

--- MainForm/Form1.cs
+++ MainForm/Form1.cs
@@ -949,7 +949,12 @@ namespace MainForm
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-            
+            myRecB.Image = Image.FromFile(ImageFileNameMyRecB);
+            favB.Image = Image.FromFile(ImageFileNameFavB);
+            addRecB.Image = Image.FromFile(ImageFileNameAddRecB);
+            settingsB.Image = Image.FromFile(ImageFileNameSettingsB);
+            helpB.Image = Image.FromFile(ImageFileNameHelpB);
+            startBox.BackgroundImage = Image.FromFile(ImageFileNameStartPage);
         }
     }
     
